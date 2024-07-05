c0c05ade33754a85f38c2a37d1071799a23b86e0
tomek834 <tomek.jukin@gmail.com>
2024-07-05 18:10:19 +0200

    logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,8 +2,14 @@
 
 
 if [[ "$1" == "--date" ]]; then
-    echo "Dzisiaj jest: $(date)"
+    echo "dzisiaj jest: $(date)"
+elif [[ "$1" == "--logs" ]]; then
+    for i in $(seq 1 100); do
+        filename="log$i.txt"
+        echo -e "filename: $filename\nscript: $0\ndate: $(date)" > $filename
+    done
+
 else
 
-    echo "zly parametr"
+    echo "usage: $0 --date | --logs"
 fi
