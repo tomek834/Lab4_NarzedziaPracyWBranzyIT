84a2b385d30ec9f5fd3ce4a27601e15709bc7a08
tomek834 <tomek.jukin@gmail.com>
2024-07-05 18:15:41 +0200

    specific number of log files

--- skrypt.sh
+++ skrypt.sh
@@ -4,7 +4,14 @@
 if [[ "$1" == "--date" ]]; then
     echo "dzisiaj jest: $(date)"
 elif [[ "$1" == "--logs" ]]; then
-    for i in $(seq 1 100); do
+
+   if [[ -n "$2" ]]; then
+        num_files=$2
+    else
+        num_files=100
+    fi
+
+    for i in $(seq 1 $num_files); do
         filename="log$i.txt"
         echo -e "filename: $filename\nscript: $0\ndate: $(date)" > $filename
     done
